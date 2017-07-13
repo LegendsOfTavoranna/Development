54b99a33810d16f5fb7ddd8ce2ca52a475c09bf5
M4V3R1CKDigital <tanner@m4v3r1ckdigital.com>
2017-07-12 23:38:31 -0400

    Created base camera movement script to test GitHub
    Its very basic camera movement, and I don't know if you already have one done or not, but I wanted to test the system.

---
+++ CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour 
+{
+	//This script gets attached to the camera gameobject.
+	
+	public GameObject player; //The player gameobject in the scene gets assinged here
+
+	void Update()
+	{
+		this.transform.position = new Vector3 (player.transform.position.x, 1.16f, -10);
+	}
+}
