2660704ad517a0baca5ebe5f019700bd2faa0de0
DrDerp20 <mirenbhakta21@gmail.com>
2017-07-14 14:11:29 -0500

    Miren's Main Menu
    My take on a menu, it is its own separate scene.

---
+++ Legends Of Tavoranna Unity Project/Assets/Scripts/Main Menu Scripts/LargeLogo.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// Goes on the large logo main menu object
+public class LargeLogo : MonoBehaviour
+{
+
+	public float fadeStartDelayLength = 3;
+	public AnimationCurve fadeCurve;
+
+	private float currentAlpha = 0;
+
+	private Image largeLogo;
+	private float length;
+
+	private void Start ()
+	{
+		
+		Keyframe lastKey = fadeCurve.keys[fadeCurve.keys.Length - 1];
+
+		length = lastKey.time + 0.5f;
+
+		largeLogo = GetComponent<Image>();
+	}
+
+	private void FixedUpdate ()
+	{
+
+		if (fadeStartDelayLength <= 0)
+		{
+
+			largeLogo.color = new Color(1, 1, 1, fadeCurve.Evaluate(currentAlpha));
+			currentAlpha += Time.fixedDeltaTime;
+		}
+		else
+		{
+
+			fadeStartDelayLength -= Time.fixedDeltaTime;
+		}
+
+		if (currentAlpha >= length)
+		{
+
+			this.gameObject.transform.parent.gameObject.SetActive(false);
+			this.gameObject.SetActive(false);
+			this.enabled = false;
+		}
+	}
+}
