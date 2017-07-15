1a3c39b9d2e0332e22bee9a1e32eafc1c183534b
DrDerp20 <mirenbhakta21@gmail.com>
2017-07-15 15:19:47 -0500

    Small things

--- Legends Of Tavoranna Unity Project/Assets/Scripts/Main Menu Scripts/MainMenuController.cs
+++ Legends Of Tavoranna Unity Project/Assets/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -4,4 +4,9 @@ public class MainMenuController : MonoBehaviour {
 
 	public LargeLogo largeLogo;
 	public PlayButton playbutton;
+
+	private void Awake () {
+
+		largeLogo.gameObject.SetActive(true);
+	}
 }
